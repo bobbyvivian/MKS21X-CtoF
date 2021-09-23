f37962632cd07cbd2ff5d73688fc0960cebffef3
bobbyvivian <vivianteo950@gmail.com>
2021-09-23 19:08:00 -0400

    tested both functions in main, worked

--- CtoFTester.java
+++ CtoFTester.java
@@ -18,4 +18,16 @@ public class CtoFTester {
     return (fahr - 32) * 5/9;
   }
 
+  public static void main(String[] args) {
+    System.out.println(celsiusToFahrenheit(0));
+    System.out.println(celsiusToFahrenheit(-40));
+    System.out.println(celsiusToFahrenheit(100));
+
+    System.out.println(fahrenheitToCelsius(32));
+    System.out.println(fahrenheitToCelsius(-40));
+    System.out.println(fahrenheitToCelsius(212));
+  }
+
+
+
 }
