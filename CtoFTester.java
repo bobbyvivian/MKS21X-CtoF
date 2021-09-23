9b7a38d6338fc9fa4fb482482d3c20de96bc6eb5
bobbyvivian <vivianteo950@gmail.com>
2021-09-23 18:54:04 -0400

    made the first function, haven't tested anything

---
+++ CtoFTester.java
@@ -0,0 +1,11 @@
+public class CtoFTester {
+
+  /* celsiusToFahrenheit()
+  parameter should be degrees in celsius as a double,
+  the function should return the temperature in fahrenheit
+  */
+
+  public static double celsiusToFahrenheit(double celsius) {
+    return (celsius * 9/5) + 32;
+  }
+}
