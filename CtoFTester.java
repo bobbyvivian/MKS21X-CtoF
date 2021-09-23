262e87a05282713f83fa15c50ec8a271d0a10c14
bobbyvivian <vivianteo950@gmail.com>
2021-09-23 19:03:32 -0400

    created the other function, haven't tested anything

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,10 +2,20 @@ public class CtoFTester {
 
   /* celsiusToFahrenheit()
   parameter should be degrees in celsius as a double,
-  the function should return the temperature in fahrenheit
+  the function should return the temperature in fahrenheit as a double
   */
 
   public static double celsiusToFahrenheit(double celsius) {
     return (celsius * 9/5) + 32;
   }
+
+  /* fahrenheitToCelsius()
+  parameter should be degrees in fahrenheit as a double,
+  the function should return the temperature in celsius as a double
+  */
+
+  public static double fahrenheitToCelsius(double fahr) {
+    return (fahr - 32) * 5/9;
+  }
+
 }
